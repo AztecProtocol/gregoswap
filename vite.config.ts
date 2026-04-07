e527efe06a124581ff188c223342a4802b493834
thunkar <gregojquiros@gmail.com>
2026-04-07 17:47:41 +0200

    chunk limit

--- vite.config.ts
+++ vite.config.ts
@@ -117,7 +117,7 @@ export default defineConfig(({ mode }) => {
       chunkSizeValidator([
         {
           pattern: /assets\/index-.*\.js$/,
-          maxSizeKB: 1550,
+          maxSizeKB: 1600,
           description: 'Main entrypoint, hard limit',
         },
         {
